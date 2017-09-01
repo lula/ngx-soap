58725ae61ff4225eed26cc1ae5c19013f4e1f33f
Lula <lucalula@gmail.com>
2017-09-01 21:22:27 +0200

    0.1.8

--- dist/src/libts/soap.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function createSoapClient(wsdlDef: any, options?: any): Promise<any>;
--- dist/src/soap.service.d.ts
+++
@@ -1,6 +0,0 @@
-import 'rxjs/add/operator/map';
-import { Client } from "./libts/client";
-export declare class SOAPService {
-    constructor();
-    createClient(wsdlDef: string, options?: any): Promise<Client>;
-}
