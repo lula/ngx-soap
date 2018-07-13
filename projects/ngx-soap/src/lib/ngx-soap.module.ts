618d95a50ca731d4a4ba14cafe524ee58fc94078
llulani <luca.lulani@trilog.it>
2018-07-13 15:32:15 +0200

    latest changes

--- projects/ngx-soap/src/lib/ngx-soap.module.ts
+++ projects/ngx-soap/src/lib/ngx-soap.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports: [],
+  imports: [
+    HttpClientModule
+  ],
   exports: []
 })
 export class NgxSoapModule { }
